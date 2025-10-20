97decdc35f364c02561139c176326f630f41f9a7
PsychoKike <al349549@edu.uaa.mx>
2025-10-20 16:36:36 -0600

    Nuevo menu

--- app/src/main/java/com/example/proyectoprogra/MainActivity.kt
+++ app/src/main/java/com/example/proyectoprogra/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.proyectoprogra // Asegúrate de que este sea tu paquete
+package com.example.proyectoprogra
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,22 +10,19 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // 1. Carga tu layout. MotionLayout se activará automáticamente.
         setContentView(R.layout.activity_main)
 
-        // 2. Busca el botón por su ID
         val startButton = findViewById<Button>(R.id.boton_iniciar)
 
-        // 3. Asigna la acción de clic
         startButton.setOnClickListener {
-            // Acción al hacer clic.
-            // Por ejemplo, mostrar un mensaje:
             Toast.makeText(this, "Iniciando Aventura...", Toast.LENGTH_SHORT).show()
 
-            // O (lo más probable) iniciar tu siguiente actividad:
-            // val intent = Intent(this, TuSiguienteActividad::class.java)
-            // startActivity(intent)
+            // Inicia la siguiente actividad
+            val intent = Intent(this, Menu1::class.java)
+            startActivity(intent)
+
+            // Agrega transición de desvanecimiento
+
         }
     }
 }
---
+++ app/src/main/java/com/example/proyectoprogra/Menu1.kt
@@ -0,0 +1,12 @@
+package com.example.proyectoprogra
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Menu1: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.menu)
+    }
+}
